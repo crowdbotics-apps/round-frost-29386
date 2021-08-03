86218f6e07a44615e89683c64fc0962fe3664bee
Crowdbotics <team@crowdbotics.com>
2021-08-03 11:11:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "round_frost_29386Identifier",
+  url: "https://round_frost_29386Identifier.botics.co",
+  api: "https://round_frost_29386Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
